c445399d5aa994721f7f62c1ecc4c2af20e9474d
Mark Judy <mrkjudy@gmail.com>
2020-06-01 15:53:12 -0400

    did some more problems

--- CounterMain.ts
+++ CounterMain.ts
@@ -1,7 +1,7 @@
 // A program to count from 0 to max using worker_threads and atomics
 
-import worker_threads from 'worker_threads';
-import path from 'path';
+import worker_threads from "worker_threads";
+import path from "path";
 
 // Take the number of workers as a command-line argument
 const numWorkers = +process.argv[2];
@@ -17,9 +17,10 @@ let numFinished = 0;
 const start = Date.now();
 // Run the workers
 for (let i = 0; i < numWorkers; i++) {
-    workers.push(new worker_threads.Worker(path.join(__dirname, 'CounterWorker.js'),
-        { workerData: {"max": max, "count": count} })
-        .on('exit', () => {
+    workers.push(
+        new worker_threads.Worker(path.join(__dirname, "CounterWorker.js"), {
+            workerData: { max: max, count: count },
+        }).on("exit", () => {
             numFinished++;
             // Print the stats if done
             if (numFinished >= numWorkers) {
---
+++ MergeTwoBinaryTrees.ts
@@ -0,0 +1,32 @@
+interface TreeNode {
+    val: number;
+    left?: TreeNode;
+    right?: TreeNode;
+}
+
+type MTreeNode = TreeNode | undefined;
+
+const mergeTrees = (t1: MTreeNode, t2: MTreeNode): MTreeNode => {
+    if (!t1 || !t2)
+        return t1 || t2;
+
+    const stack = Array<[MTreeNode,MTreeNode]>();
+    stack.push([t1, t2]);
+    while (stack.length > 0) {
+        const pair = stack.pop();
+        if (!pair)
+            break;
+        if (!pair[0] || !pair[1])
+            continue;
+        pair[0].val += pair[1].val;
+        if (!pair[0].left)
+            pair[0].left = pair[1].left;
+        else
+            stack.push([pair[0].left, pair[1].left]);
+        if (!pair[0].right)
+            pair[0].right = pair[1].right;
+        else
+            stack.push([pair[0].right, pair[1].right]);
+    }
+    return t1;
+}
