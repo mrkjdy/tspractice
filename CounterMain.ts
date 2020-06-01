2e6411b5e0873bb6d6076cb396a10223c037efc3
Mark Judy <mrkjudy@gmail.com>
2020-05-31 22:48:14 -0400

    Added optimized cpp results

--- CounterMain.ts
+++ CounterMain.ts
@@ -34,12 +34,12 @@ for (let i = 0; i < numWorkers; i++) {
 // Benchmark results counting to 10000000
 // (Ran on a 6600k)
 // CounterMain.ts
-// Number of workers    CounterMain.ts  main.cpp
-// 1                    0.342           0.240
-// 2                    0.656           0.608
-// 4                    0.859           0.863
-// 8                    0.936           0.868
-// 16                   1.041           0.861
-// 32                   1.307           0.849
+// Number of workers    CounterMain.ts  main.cpp(-O0)   main.cpp(-O2)
+// 1                    0.342           0.240           0.091
+// 2                    0.656           0.608           0.393
+// 4                    0.859           0.863           0.665
+// 8                    0.936           0.868           0.653
+// 16                   1.041           0.861           0.656
+// 32                   1.307           0.849           0.614
 
 // Looks like there is a lot of contention!
